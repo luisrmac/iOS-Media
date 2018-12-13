ddaa1c07d9d591f87a9fc4b0ca87d3435ac4a305
Jose Luis Romero <admin@Joses-MacBook-Pro.local>
2018-12-13 11:09:23 -0500

    Initial Commit

---
+++ Media/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Media
+//
+//  Created by Jose Luis Romero on 12/13/18.
+//  Copyright © 2018 Jose Luis Romero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
